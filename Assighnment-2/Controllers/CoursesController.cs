4b9106d47d9e124d7ce61bf7e32d1d1aa13bdcbb
MrESB <mr.abolfazlbeigi@gmail.com>
2025-10-29 22:00:13 +0330

    CoursesController Added

---
+++ Assighnment-2/Controllers/CoursesController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using web_assignment_2.Data;
+using web_assignment_2.Models;
+
+namespace web_assignment_2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        private readonly SchoolContext _context;
+
+        public CourseController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Courses>>> GetCourses()
+        {
+            return await _context.Courses.Include(c => c.Teacher).Include(c => c.Enrollments).ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Courses>> GetCourse(int id)
+        {
+            var course = await _context.Courses
+                .Include(c => c.Teacher)
+                .Include(c => c.Enrollments)
+                .ThenInclude(e => e.Student)
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+            if (course == null) return NotFound();
+            return course;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Courses>> PostCourse(Courses course)
+        {
+            _context.Courses.Add(course);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetCourse), new { id = course.Id }, course);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCourse(int id, Courses course)
+        {
+            if (id != course.Id) return BadRequest();
+            _context.Entry(course).State = EntityState.Modified;
+
+            try { await _context.SaveChangesAsync(); }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CourseExists(id)) return NotFound();
+                else throw;
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCourse(int id)
+        {
+            var course = await _context.Courses.FindAsync(id);
+            if (course == null) return NotFound();
+            _context.Courses.Remove(course);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        private bool CourseExists(int id) => _context.Courses.Any(c => c.Id == id);
+    }
+}
